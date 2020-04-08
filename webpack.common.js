ba6add780bd750f4f29595af26ce65cb423b96a9
Eddie Anguiano <eddiesanguiano@gmail.com>
2020-04-08 14:11:25 -0700

    initial commit

---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", //3. Inject styles into DOM
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader" //1. Turns sass into css
+        ]
+      }
+    ]
+  }
+};
